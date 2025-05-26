e4f4ed41e455903b2995908282773a8ef169c74a
Vinicius-LO <vi.lugli.programacao@gmail.com>
2025-05-25 21:49:04 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+let lista=[]
+let tentativas = 1;
+let numeroMaximo = 10
+let numeroSecreto=gerarNumeroAleatorio()
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroMaximo}`);
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', `O número secreto é menor que ${chute}`);
+        } else {
+            exibirTextoNaTela('p', `O número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroMaximo + 1);
+    let quantiElementos = lista.length
+
+    if (quantiElementos==numeroMaximo){
+        lista=[]
+    }
+
+    if(lista.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio()
+    } else{
+        lista.push(numeroEscolhido)
+        return numeroEscolhido
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true)
+}
